e8f0329404cbc8412bc992fdc8df7ec92dc88b66
agiletkiewicz <a.letkiewicz@gmail.com>
2021-02-26 14:15:15 -0800

    add list form successfully posts to db

--- src/components/lists/ListForm.tsx
+++ src/components/lists/ListForm.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 const ListForm = () => {
 
+    const API = "http://localhost:3000/lists"
+
     const [name, setName] = useState("");
 
     function onChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -10,15 +12,26 @@ const ListForm = () => {
 
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        console.log(name);
+
+        fetch(API, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'}, 
+            body: JSON.stringify({name: name})
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+        })
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>List name</label>
-            <input name={name} onChange={onChange} id="name" type="text"/>
-            <input type="submit"/>
-        </form>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>List name</label>
+                <input name={name} onChange={onChange} id="name" type="text"/>
+                <input type="submit"/>
+            </form>
+        </div>
     )
 
 };
